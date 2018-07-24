ab5fb2b71b87f17d8184f48ac1b5ddc611fddcc5
sonyapieklik <piesonya@gmail.com>
2018-07-24 19:10:28 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -12,19 +12,31 @@ def eur_to_usd(amount)
 end
 
 def usd_to jpy(amount)
+  total = amount *3
+  return total
 end
 
 def usd_to_gbp(amount)
+  total = amount *3
+  return total
 end
 
 def usd_to_aud(amount)
+  total = amount *3
+  return total
 end
 
 def jpy_to_usd(amount)
+  total = amount *3
+  return total
 end
 
 def gbp_to_usd(amount)
+  total = amount *3
+  return total
 end
 
 def aud_to_usd(amount)
+  total = amount *3
+  return total
 end
