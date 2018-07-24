b89b790d62990f4ddbe94b2cf16610675d645ca4
sonyapieklik <piesonya@gmail.com>
2018-07-24 19:11:15 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,7 +1,19 @@
-def usd_to_eur(amount)
-  total = amount * 1.2 
-  return total
-end
+
+
+
+
+puts "How much money do you want to convert?"
+amount = gets.chomp().to_i
+ 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
   
   
   
@@ -37,6 +49,5 @@ def gbp_to_usd(amount)
 end
 
 def aud_to_usd(amount)
-  total = amount *3
-  return total
+  
 end
